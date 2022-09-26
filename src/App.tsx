9268de801771790ac7711aae1ff1cb2185d21b69
Patryk Marczak <m.patryk@outlook.com>
2022-09-26 20:39:08 +0200

    Fetching departments & science clubs

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { fetcher } from './utils/fetcher';
+import { URLHandler } from './utils/consts';
 
 function App() {
+  const [departments, setDepartments] = useState<string[]>([]);
+
+  const [scienceClubs, setScienceClubs] = useState<string[]>([]);
+
+  useEffect(() => {
+    const getDepartments = async () => {
+      const departments = await fetcher(URLHandler.getDepartmentsUrl);
+      setDepartments(departments);
+    };
+
+    const getScienceClubs = async () => {
+      const scienceClubs = await fetcher(URLHandler.getClubsUrl);
+      setScienceClubs(scienceClubs);
+    };
+
+    getDepartments().catch((err) => console.log(err));
+    getScienceClubs().catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className='App'>
       <header className='App-header'>
