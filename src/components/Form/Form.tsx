4a1268a907b81b3e75a861b5d81017f05172e1ea
Krzysztof Zduniak <krzysz.zdun@gmail.com>
2022-09-29 11:10:42 +0200

    Remove inputType prop from FieldInput

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -11,31 +11,31 @@ export const Form = ({ departments, scienceClubs }: Props) => {
     <div className={styles.formBox}>
       <form>
         <section className={styles.formSection}>
-        <h2 className={styles.header}>Dane koła</h2>
-        <GeneralInput label='Przewodniczący koła naukowego: '>
-          <FieldInput inputType='text' />
-        </GeneralInput>
-        <GeneralInput label='Nazwa koła naukowego: '>
-          <FieldSelect options={scienceClubs} />
-        </GeneralInput>
-        <GeneralInput label='Nazwa wydziału:'>
-          <FieldSelect options={departments} />
-        </GeneralInput>
-        <GeneralInput label='Opiekun koła (wraz z tytułem/tytułami):'>
-          <FieldInput inputType='text' />
-        </GeneralInput>
+          <h2 className={styles.header}>Dane koła</h2>
+          <GeneralInput label='Przewodniczący koła naukowego: '>
+            <FieldInput />
+          </GeneralInput>
+          <GeneralInput label='Nazwa koła naukowego: '>
+            <FieldSelect options={scienceClubs} />
+          </GeneralInput>
+          <GeneralInput label='Nazwa wydziału:'>
+            <FieldSelect options={departments} />
+          </GeneralInput>
+          <GeneralInput label='Opiekun koła (wraz z tytułem/tytułami):'>
+            <FieldInput />
+          </GeneralInput>
         </section>
         <section className={styles.formSection}>
-        <h2 className={styles.header}>Dane indywidualne</h2>
-        <GeneralInput label='Imię i nazwisko: '>
-          <FieldInput inputType='text' />
-        </GeneralInput>
-        <GeneralInput label='Indeks'>
-          <FieldInput inputType='text' />
-        </GeneralInput>
-        <GeneralInput label='Funkcja w kole'>
-          <FieldInput inputType='text' />
-        </GeneralInput>
+          <h2 className={styles.header}>Dane indywidualne</h2>
+          <GeneralInput label='Imię i nazwisko: '>
+            <FieldInput />
+          </GeneralInput>
+          <GeneralInput label='Indeks:'>
+            <FieldInput />
+          </GeneralInput>
+          <GeneralInput label='Funkcja w kole:'>
+            <FieldInput />
+          </GeneralInput>
         </section>
       </form>
     </div>
--- src/components/Form/Inputs/FieldInput.tsx
+++ src/components/Form/Inputs/FieldInput.tsx
@@ -2,14 +2,13 @@ import styles from '../Form.module.scss';
 import React from 'react';
 
 interface Props {
-  inputType: string;
   isTextArea?: boolean;
 }
 
-export function FieldInput({ inputType, isTextArea }: Props) {
+export function FieldInput({ isTextArea }: Props) {
   return isTextArea ? (
     <textarea></textarea>
   ) : (
-    <input className={styles.input} type={inputType}></input>
+    <input className={styles.genericInput} type='text'></input>
   );
 }
